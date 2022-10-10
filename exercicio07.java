0be66d0832a7e306189168c36a0204ebf9781af5
vitors12 <115507870+vitors12@users.noreply.github.com>
2022-10-10 19:31:48 -0300

    Add files via upload

---
+++ exercicio07.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class exercicio07 {
+    public static void main (String [] args) {
+        double lado, area, dobroArea;
+        Scanner ler = new Scanner (System.in);
+        lado = 15;
+        area = 15 * 15;
+        dobroArea = area * 2;
+        System.out.println("O valor do dobro da area do quadrado e " + dobroArea);
+}
+}
